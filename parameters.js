0562100f57574ba4c75855badf79b83e66f111d2
gaenuc <gaenuc@gmail.com>
2022-11-03 12:11:13 +0100

    update-device

---
+++ parameters.js
@@ -0,0 +1,7 @@
+module.exports = {
+sender: '0xf742983449F6c642F1ab5F7B595767166604aaBc',
+receiver: '0xe8b29724Fb9F3c29E8E0316aD97B85C82ea91C95',
+http: 'https://eth-ropsten.alchemyapi.io/v2/VP6B0a8K2g2tsmudZMLbvQXvR8_M_ukw',
+webSocket: 'wss://eth-ropsten.ws.alchemyapi.io/v2/VP6B0a8K2g2tsmudZMLbvQXvR8_M_ukw',
+privateKey: '67245710a999c21b767f9de7b5921cc52542a7c1728b436a18d3922e84eda76e',
+}
